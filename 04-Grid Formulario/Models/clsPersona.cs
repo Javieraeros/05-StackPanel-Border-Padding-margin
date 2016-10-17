6d4a08d7b573ac544031ccb33325e86e27764403
Francisco Javier Ruiz Rodriguez <Pajarrurro@gmail.com>
2016-10-17 14:19:37 +0200

    Cambios

---
+++ 04-Grid Formulario/Models/clsPersona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Grid_Formulario.Models
+{
+    class clsPersona
+    {
+    }
+}
